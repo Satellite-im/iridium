9311a108b424c3c05ddd4fbceaccdd1b45c4f056
mtumiati <manuel.tumiati@knobs.it>
2022-06-09 15:04:38 +0200

    chore: update rollup config to emit declaration file

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,38 @@ import esbuild from 'rollup-plugin-esbuild';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
+const config = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+    },
+    plugins: [
+      resolve({ browser: true, preferBuiltins: false }),
+      json(),
+      commonjs({
+        include: 'node_modules/**',
+        ignoreGlobal: true,
+      }),
+      esbuild({
+        include: /\.[jt]s?$/,
+        target: 'esnext',
+        tsconfig: 'tsconfig.json',
+        loaders: {
+          '.json': 'json',
+        },
+      }),
+      dts(),
+    ],
   },
-  plugins: [
-    resolve({ browser: true, preferBuiltins: false }),
-    json(),
-    commonjs({
-      include: 'node_modules/**',
-      ignoreGlobal: true,
-    }),
-    esbuild({
-      include: /\.[jt]s?$/,
-      target: 'esnext',
-      tsconfig: 'tsconfig.json',
-      loaders: {
-        '.json': 'json',
-      },
-    }),
-  ],
-};
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
