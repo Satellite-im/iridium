31d3ad99bdc20391857f9065a9e03fc0a4ce777b
Drew Ewing <drew.ewing@satellite.im>
2022-06-09 16:29:49 -0700

    feat(iridium): browser & node builds

--- src/readline-adapter.ts
+++ src/readline-adapter.ts
@@ -1,3 +1,4 @@
+import * as json from 'multiformats/codecs/json';
 import minimist from 'minimist';
 import Iridium from './iridium';
 import type { IridiumMessage } from './iridium';
@@ -6,6 +7,8 @@ import Emitter from './emitter';
 export type IridiumTerminalCommands = {
   [key: string]: (...args: any[]) => any;
 };
+
+const textEncoder = new TextEncoder();
 
 export default class IridiumTerminal extends Emitter<IridiumMessage> {
   private _commands: IridiumTerminalCommands;
@@ -54,15 +57,15 @@ export default class IridiumTerminal extends Emitter<IridiumMessage> {
       ...props
     } = minimist(line.split(' '));
     const command = this.command(cmd);
-    args.map((arg) =>
-      props.json
-        ? JSON.parse(arg)
+    const input = args.map((arg) =>
+      arg.startsWith('enc:')
+        ? textEncoder.encode(arg.substring(4))
         : arg.startsWith('json:')
         ? JSON.parse(arg.substring(5))
         : arg
     );
     try {
-      const result = await command(...args);
+      const result = await command(...input);
       console.info(JSON.stringify(result, null, 2));
     } catch (e) {
       console.error(e);
