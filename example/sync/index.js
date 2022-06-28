cd0edebf6477553d2c8750d34a48a49ed8e4ba74
Drew Ewing <drew.ewing@satellite.im>
2022-06-28 13:17:30 -0700

    feat(sync-node): rough draft pinning & offline sync from stash

---
+++ example/sync/index.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({
+  path: '../../.env',
+});
+
+import IridiumSyncAgent from '../../dist/sync-agent';
+
+const agent = await IridiumSyncAgent.create('sync node seed');
