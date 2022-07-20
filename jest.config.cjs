622400e84de45ac97f0cecb7cb55dd255918f766
stavares <saratavares843@gmail.com>
2022-07-20 17:41:05 +0100

    chore(jest): add jest

---
+++ jest.config.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/*.config.js',
+    '!**/*.config.ts',
+    '!**/config.ts',
+    '!**/startup.js',
+    '!**/*.eslintrc.js',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^[@|~]/(.*)$': '<rootDir>/$1',
+    skaler: '<rootDir>/store/textile/__mocks__/skaler.js',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/.nuxt',
+    '<rootDir>/cypress',
+    '<rootDir>/coverage',
+    '<rootDir>/plugins',
+    '<rootDir>/electron',
+    '<rootDir>/libraries/Enums',
+    '<rootDir>/libraries/Files/errors',
+    '<rootDir>/libraries/Files/types',
+    '<rootDir>/types',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    'node_modules/(?!@mylibrary/)',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+}
